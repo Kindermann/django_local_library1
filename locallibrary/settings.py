1c911c8c3232a12046b8232f53ef055a68250ca7
root <s9295607@mail.zih.tu-dresden.de>
2019-03-17 01:39:42 -0400

    setting of allowed_hosts in settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dry-cliffs-53618.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
